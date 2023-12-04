af1062dfe860297809a21ef5e0e9f460ae5ffe80
minaj27 <minaj13m@gmail.com>
2023-12-04 23:51:55 +0530

    first commit

---
+++ profit.js
@@ -0,0 +1,11 @@
+document.write("<h1>Profit Calculator</h1>");
+
+var cp = prompt("Enter the cost price :");
+
+var sp =prompt("Enter the selling Price :");
+
+var profit  = sp-cp;
+
+document.write("Total Profit :");
+
+document.write(profit);
